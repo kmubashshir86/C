91c41e8ce58c7f817d26215b28c085d3664ba79e
kmubashshir86 <kmubasshir7678387556@gmail.com>
2026-02-21 07:30:19 +0530

    q56.c updated +q57.c

--- questions/Q56/q56.c
+++ questions/Q56/q56.c
@@ -5,10 +5,10 @@ int main(){
     int length;
     scanf("%d",&length);
     printf("enter string : ");
-    char str[(length-1)];
+    char str[length];
     scanf("%s",&str);
     int vowel = 0,consonant =0;
-    for (int i=0;i<=(length-1);i++){
+    for (int i=0;i<=length;i++){
         if ((str[i]<='Z' && str[i]>='A') || (str[i]<='z' && str[i]>='a')){
             if((str[i] == 'a' ||str[i] == 'e' ||str[i] == 'i' || str[i] == 'o' ||str[i] == 'u') || ((str[i] == 'A' ||str[i] == 'E' ||str[i] == 'I' || str[i] == 'O' ||str[i] == 'U'))){
                 vowel+=1;
